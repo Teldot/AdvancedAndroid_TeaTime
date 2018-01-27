515674d4acb7226729b2dc63931d8af678a7a0ee
teldot <teldot@gmail.com>
2018-01-27 15:47:47 -0800

    -Implemented ImageDownloader.DelayerCallback. -Added a SimpleIdlingResource variable that will be null in production. -Created a method that returns the IdlingResource variable. -Got the IdlingResource variable. -Overrided onDone so when the thread in ImageDownloader is finished. -Registered any resource that needs to be synchronized with Espresso. -Tested that the gridView with Tea objects appears and we can click a gridView item. -Unregistered resources when not needed to avoid malfunction.

--- app/src/androidTest/java/com/example/android/teatime/IdlingResourceMenuActivityTest.java
+++ app/src/androidTest/java/com/example/android/teatime/IdlingResourceMenuActivityTest.java
@@ -17,6 +17,7 @@
 package com.example.android.teatime;
 
 
+import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -27,15 +28,20 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.anything;
+
 /**
  * Usually Espresso syncs all view operations with the UI thread as well as AsyncTasks, but it can't
  * do so with custom resources (e.g. activity or service). For such cases, we can register the
  * custom resource and Espresso will wait for the resource to be idle before
  * executing a view operation.
- *
+ * <p>
  * In this example, we simulate an idling situation. This test is the same as the
  * MenuActivityScreenTest but with an Idling Resource to help with synchronization.
- *
+ * <p>
  * We added an idling period from when the user clicks on a GridView item
  * in MenuActivity to when corresponding order activity appears. This is to simulate potential
  * delay that could happen if this data were being retrieved from the web. Without registering the
@@ -51,7 +57,7 @@ public class IdlingResourceMenuActivityTest {
      * The ActivityTestRule is a rule provided by Android used for functional testing of a single
      * activity. The activity that will be tested, MenuActivity in this case, will be launched
      * before each test that's annotated with @Test and before methods annotated with @Before.
-     *
+     * <p>
      * The activity will be terminated after the test and methods annotated with @After are
      * complete. This rule allows you to directly access the activity during the test.
      */
@@ -62,22 +68,24 @@ public class IdlingResourceMenuActivityTest {
     private IdlingResource mIdlingResource;
 
 
-    // TODO (6) Registers any resource that needs to be synchronized with Espresso before
+    // COMPLETED (6) Registers any resource that needs to be synchronized with Espresso before
     // the test is run.
     @Before
     public void registerIdlingResource() {
-
+        mIdlingResource = mActivityTestRule.getActivity().getIdlingResource();
+        Espresso.registerIdlingResources(mIdlingResource);
     }
 
-    // TODO (7) Test that the gridView with Tea objects appears and we can click a gridView item
+    // COMPLETED (7) Test that the gridView with Tea objects appears and we can click a gridView item
     @Test
     public void idlingResourceTest() {
-
+        onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(0).perform(click());
     }
 
-    // TODO (8) Unregister resources when not needed to avoid malfunction
+    // COMPLETED (8) Unregister resources when not needed to avoid malfunction
     @After
     public void unregisterIdlingResource() {
-
+        if (mIdlingResource != null)
+            Espresso.unregisterIdlingResources(mIdlingResource);
     }
 }
